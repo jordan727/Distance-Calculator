d3e5312d1cdb197497e47fcff265bd7cc13bc7ba
jordan727 <imjordanantonio@gmail.com>
2022-06-09 09:58:41 -0600

    Init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Distance Calculator by Jordan A
+#nullable disable
+Console.Clear();
+
+// Welcome
+Console.WriteLine("Welcome to the Distance Calculator");
+
+// Input
+Console.Write("Enter x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Output
+double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine($"Distance between points is {distance}.");
